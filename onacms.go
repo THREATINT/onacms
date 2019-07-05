193ebbddb1f2f9b5614e37041e140985e6b52530
Stefan Beyer <sbeyer@threatint.com>
2019-07-05 18:16:30 +0200

    initial version

---
+++ onacms.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"net/http"
+	"os"
+	"os/user"
+	"strings"
+	"time"
+
+	"github.com/NYTimes/gziphandler"
+	"github.com/THREATINT/onacms/core"
+	"github.com/rs/zerolog"
+	"github.com/spf13/afero"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+func main() {
+	var (
+		dir  = kingpin.Flag("dir", "directory containing the site (/public /nodes /templates)").Required().String()
+		port = kingpin.Flag("port", "(optional) TCP port").Default("10000").Int16()
+		//logformat   = kingpin.Flag("log-format", "(option) set to JSON for e.g.logstash").String()
+		//application = kingpin.Flag("application", "(option) used for logging").String()
+		//instance    = kingpin.Flag("instance", "(option) used for logging").String()
+		//debug       = kingpin.Flag("debug", "(optional) log debug messages").Default("false").Bool()
+	)
+
+	kingpin.Parse()
+
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	output.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("| %-6s|", i))
+	}
+	log := zerolog.New(output).With().Timestamp().Logger()
+
+	log.Info().Msg("onacms (C) 2017 - 2019 THREATINT")
+
+	u, err := user.Current()
+	if err == nil && u.Username == "root" {
+		log.Warn().Msg("please do not run as root!")
+	}
+
+	fs := afero.NewBasePathFs(afero.NewOsFs(), *dir)
+
+	core := core.NewCore(&fs, log)
+
+	if len(core.Nodes) == 0 {
+		log.Fatal().Msg("no nodes, exiting...")
+		os.Exit(0xe0)
+	}
+
+	log.Info().Msg(fmt.Sprintf("starting onacms port %d", *port))
+
+	defer func() {
+		if r := recover(); r != nil {
+			log.Fatal().Msg(fmt.Sprintf("%+v - exiting", r))
+			os.Exit(0xfe)
+		}
+	}()
+
+	server := http.NewServeMux()
+
+	gzh, err := gziphandler.GzipHandlerWithOpts(
+		gziphandler.MinSize(10),
+		gziphandler.CompressionLevel(gzip.BestCompression))
+
+	server.Handle("/", gzh(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		core.HttpGet(w, r)
+	})))
+
+	err = http.ListenAndServe(fmt.Sprintf(":%d", *port), server)
+	if err != nil {
+		log.Fatal().Msg(fmt.Sprintf("%s - exiting", err.Error()))
+		os.Exit(0xfc)
+	}
+}
