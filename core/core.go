de6d6c496b253de9a9cc443e0b8f6def38aa7c52
Stefan Beyer <sbeyer@threatint.com>
2019-12-29 22:25:03 +0100

    wcwip

--- core/core.go
+++ core/core.go
@@ -20,11 +20,11 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/spf13/afero"
 	"github.com/tdewolff/minify/v2"
-	"github.com/tdewolff/minify/v2/svg"
 	"github.com/tdewolff/minify/v2/css"
 	"github.com/tdewolff/minify/v2/html"
 	"github.com/tdewolff/minify/v2/js"
 	"github.com/tdewolff/minify/v2/json"
+	"github.com/tdewolff/minify/v2/svg"
 	"github.com/tdewolff/minify/v2/xml"
 )
 
