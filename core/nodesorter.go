f3f1d43803ed707d1a15b54c02f2c41d4a95d876
Stefan Beyer <sbeyer@threatint.com>
2020-11-01 12:44:21 +0100

    added comments, wcwip

--- core/nodesorter.go
+++ core/nodesorter.go
@@ -1,15 +1,19 @@
 package core
 
+// NodeSorter type
 type NodeSorter []*Node
 
+// Len return length of node list
 func (n NodeSorter) Len() int {
 	return len(n)
 }
 
+// Swap swap position of two nodes
 func (n NodeSorter) Swap(i, j int) {
 	n[i], n[j] = n[j], n[i]
 }
 
+// Less compare weight of two nodes taking into consideration weight or, if it does not exist, creation time
 func (n NodeSorter) Less(i, j int) bool {
 	if n[i].Weight() != -1 && n[j].Weight() != -1 {
 		// both nodes have a weight
