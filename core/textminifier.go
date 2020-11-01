de5dc9f7a0bbf1402cbcbd3a74f0a23e4fb77bc8
Stefan Beyer <sbeyer@threatint.com>
2020-11-01 11:41:59 +0100

    added comments, wcwip

--- core/textminifier.go
+++ core/textminifier.go
@@ -8,14 +8,19 @@ import (
 	"github.com/tdewolff/minify/v2"
 )
 
+// TextMinifier struct
 type TextMinifier struct{}
 
+// TextMinify TextMinifier
 var TextMinify TextMinifier
 
+// Minify ()
 func Minify(m *minify.M, w io.Writer, r io.Reader, params map[string]string) error {
 	return TextMinify.Minify(m, w, r, params)
 }
 
+// Minify (m, w, r, params)
+// see https://github.com/tdewolff/minify/blob/master/minify.go for details
 func (c *TextMinifier) Minify(m *minify.M, w io.Writer, r io.Reader, params map[string]string) error {
 	b, err := ioutil.ReadAll(r)
 	if err != nil {
