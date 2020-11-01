8f3cb3b9f5f890fbded27043d2b37f84d7ba5e24
Stefan Beyer <sbeyer@threatint.com>
2020-11-01 12:31:04 +0100

    added comments, wcwip

--- core/httpheaders.go
+++ core/httpheaders.go
@@ -7,19 +7,23 @@ import (
 	"github.com/bmatcuk/doublestar"
 )
 
+// URI struct mapping header/expression
 type URI struct {
 	Expression string   `xml:"expression,attr"`
 	Header     []string `xml:"header"`
 }
 
+// HTTPHeaders struct
 type HTTPHeaders struct {
 	URI []URI `xml:"uri"`
 }
 
+// Read read http headers from []byte
 func (h *HTTPHeaders) Read(r []byte) error {
 	return xml.Unmarshal(r, &h)
 }
 
+// Match return matching headers for slug
 func (h *HTTPHeaders) Match(slug string) []string {
 	var result []string
 	slug = strings.ToLower(slug)
